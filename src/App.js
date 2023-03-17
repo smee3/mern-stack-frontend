c083c74ca598b16a12df1c5dae4bef32c9cb2356
herrerasaurus <blue777ysk@gmail.com>
2023-03-18 01:25:19 +0900

    アイテムデータを編集するページ

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/user/login"
 import ReadAll from "./pages/item/readAll"
 import ReadSingle from "./pages/item/readSingle"
 import Create from "./pages/item/create"
+import Update from "./pages/item/update"
 import "./App.css"
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path="/" element={<ReadAll />} />
                 <Route path="/item/:id" element={<ReadSingle />} />
                 <Route path="/item/create" element={<Create />} />
+                <Route path="/item/update/:id" element={<Update />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/item/update.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+const UpdateItem = () => {
+    const params = useParams()
+
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        const getSingleItem = async () => {
+            const response = await fetch(`http://localhost:5000/item/${params.id}`)
+            const jsonResponse = await response.json()
+            setTitle(jsonResponse.singleItem.title)
+            setPrice(jsonResponse.singleItem.price)
+            setImage(jsonResponse.singleItem.image)
+            setDescription(jsonResponse.singleItem.description)
+        }
+        getSingleItem()
+    }, [params.id])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`http://localhost:5000/item/update/${params.id}`, {
+                method: "PUT",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+                body: JSON.stringify({
+                    title: title,
+                    price: price,
+                    image: image,
+                    description: description
+                })
+            })
+            const jsonData = await response.json()
+            alert(jsonData.message)
+        } catch (err) {
+            alert("アイテム編集失敗")
+        }
+    }
+
+    return (
+        <div>
+            <h1>アイテム編集</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="アイテム名" required />
+                <input value={price} onChange={(e) => setPrice(e.target.value)} type="text" name="price" placeholder="価格" required />
+                <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required />
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" rows="15" placeholder="商品説明" required />
+                <button>編集</button>
+            </form>
+        </div>
+    )
+}
+
+export default UpdateItem
